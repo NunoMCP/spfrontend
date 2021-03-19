c8b4eedadb6725bd20936b2d5fee498db2a73be7
NunoMCP <nunomcp1998@gmail.com>
2021-03-19 15:02:03 +0000

    action types enum e direct action call
    falta success e fail calls em clear track actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,57 @@
+import "./actionTypes"
+import {
+    GET_TRACKS_REQUEST,
+    GET_TRACKS_SUCCESS,
+    GET_TRACKS_FAIL,
+    GET_TRACK_REQUEST,
+    GET_TRACK_SUCCESS,
+    GET_TRACK_FAIL,
+    CLEAR_TRACK_REQUEST
+} from "./actionTypes"
+
 export const getTracks = () => {
     return {
-        type: "GET_TRACKS"
+        type: GET_TRACKS_REQUEST
+    }
+}
+
+export const getTracksSuccess = (response) => {
+    return {
+        type: GET_TRACKS_SUCCESS,
+        payload: response.data
+    }
+}
+
+export const getTracksFail = (error) => {
+    return {
+        type: GET_TRACKS_FAIL,
+        payload: error.message
     }
 }
 
 export const getTrack = (id) => {
     return {
-        type: "GET_TRACK",
+        type: GET_TRACK_REQUEST,
         payload: id
+    }
+}
+
+export const getTrackSuccess = (response) => {
+    return {
+        type: GET_TRACK_SUCCESS,
+        payload: response.data
+    }
+}
+
+export const getTrackFail = (error) => {
+    return {
+        type: GET_TRACK_FAIL,
+        payload: error.message
     }
 }
 
 export const clearTrack = () => {
     return {
-        type: "CLEAR_TRACK"
+        type: CLEAR_TRACK_REQUEST
     }
 }
--- src/sagas.js
+++ src/sagas.js
@@ -1,30 +1,36 @@
 import {all, call, put, takeEvery} from "redux-saga/effects"
 import itunes from "./api/itunes"
+import {
+    getTracksSuccess,
+    getTracksFail,
+    getTrackSuccess,
+    getTrackFail,
+} from "./actions"
 
 function* fetchTracks() {
     try{
         const response = yield call(itunes.get, "search?term=rock&media=music")
-        yield put({type: "GET_TRACKS_SUCCESS", payload: response.data})
-    } catch (e) {
-        yield put({type: "GET_TRACKS_FAIL", payload: e.message})
+        yield put(getTracksSuccess(response))
+    } catch (error) {
+        yield put(getTracksFail(error))
     }
 }
 
 function* fetchTrack(action){
     try{
         const response = yield itunes.get(`lookup?id=${action.payload}`)
-        yield put({type: "GET_TRACK_SUCCESS", payload: response.data})
-    } catch (e) {
-        yield put({type: "GET_TRACK_FAIL", payload: e.message})
+        yield put(getTrackSuccess(response))
+    } catch (error) {
+        yield put(getTrackFail(error))
     }
 }
 
 function* fetchTracksWatcher(){
-    yield takeEvery("GET_TRACKS", fetchTracks)
+    yield takeEvery("GET_TRACKS_REQUEST", fetchTracks)
 }
 
 function * fetchTrackWatcher(){
-    yield takeEvery("GET_TRACK", fetchTrack)
+    yield takeEvery("GET_TRACK_REQUEST", fetchTrack)
 }
 
 function* mySaga(){
